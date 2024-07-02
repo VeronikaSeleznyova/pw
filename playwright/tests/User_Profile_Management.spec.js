20964911b360bc416b930e63be160c3b9c5d97fc
Veronika_Seleznyova <vsel0438@gmail.com>
2024-07-02 21:06:40 +0300

    Covered user profile management mod.

--- playwright/tests/User_Profile_Management.spec.js
+++ playwright/tests/User_Profile_Management.spec.js
@@ -23,34 +23,95 @@ test.describe('<Accounts>', () => {
   });
 
 
-
   test('navigate through the User Profile Management', async ({ page }) => {
-    await userManagementPage.goToUserProfileManagement();
-    await userManagementPage.goToPreferences();
-    await userManagementPage.goToApiKeys();
-    await userManagementPage.goToHome();
-    await userManagementPage.goToApiKeysFromText();
-    await userManagementPage.goToHomeFromNavigation();
-    await userManagementPage.goToPreferencesFromText();
-    await userManagementPage.goToHomeFromList();
+    await page.locator('div').filter({ hasText: /^User Profile Management$/ }).click();
+    const userElement = page.getByRole('heading', { name: 'User Profile' });
+    await expect(userElement).toBeVisible();
+    await page.getByRole('link', { name: 'Preferences' }).click();
+    const preferencesElement = page.getByRole('heading', { name: 'User Preferences' });
+    await expect(preferencesElement).toBeVisible();
+    await page.getByRole('link', { name: 'API Keys' }).click();
+    const keyElement = page.getByRole('heading', { name: 'User API Keys' });
+    await expect(keyElement).toBeVisible();
+    await page.getByRole('link', { name: 'Home' }).first().click();
+    await expect(userElement).toBeVisible();
+    await page.getByText('Api keys:').click();
+    await expect(keyElement).toBeVisible();
+    await page.getByRole('navigation').getByRole('link', { name: 'Home' }).click();
+    await expect(userElement).toBeVisible();
+    await page.getByText('Preferences:').click();
+    await expect(preferencesElement).toBeVisible();
+    await page.getByRole('list').getByRole('link', { name: 'Home' }).click();
+    await expect(userElement).toBeVisible();
   });
 
 
-  test('clear All Preferences the recent services', async ({ page }) => {
+  test('clear all preferences (pinned accounts) use CAP btn and actions', async ({ page }) => {
     await accountManagementPage.goToAccountManagement();
     await accountManagementPage.pinAccount(2);
     await accountManagementPage.pinAccount(42);
     await accountManagementPage.pinAccount(40);
     await accountManagementPage.goToHome();
     await userManagementPage.goToUserProfileManagement();
+    const userElement = page.getByRole('heading', { name: 'User Profile' });
+    await expect(userElement).toBeVisible();
     await userManagementPage.goToPreferences();
+    const userPreferencesElement = page.getByRole('heading', { name: 'User Preferences' });
+    await expect(userPreferencesElement).toBeVisible();
+    const element1 = page.getByTestId('app-main-container').getByText('- Test A12');
+    await expect(element1).toBeVisible();
+    const element2 = page.getByText('- User Admin Account TEST');
+    await expect(element2).toBeVisible();
+    const element3 = page.getByText('- new test for Andrew 22');
+    await expect(element3).toBeVisible();
+    await page.getByRole('row', { name: '- User Admin Account TEST' }).getByRole('img').click();
+    await expect(element2).not.toBeVisible();
     await userManagementPage.clearAllPreferences();
+    await expect(element1).not.toBeVisible();
+    await expect(element3).not.toBeVisible();
+    const pinnedAccountsNoData = page.locator('div').filter({ hasText: /^Pinned AccountsNameActionsNo Data$/ }).locator('h3', { hasText: 'No Data' });
+    await expect(pinnedAccountsNoData).toBeVisible();
   });
+
+
+test('clear recent services in the pteferences use actions', async ({ page }) => {
+   await page.locator('div').filter({ hasText: /^User Profile Management$/ }).click();
+   const userElement = page.getByRole('heading', { name: 'User Profile' });
+   await expect(userElement).toBeVisible();
+   await page.getByRole('link', { name: 'Preferences' }).click();
+   const userPreferencesElement = page.getByRole('heading', { name: 'User Preferences' });
+   await expect(userPreferencesElement).toBeVisible();
+   const userProfileManagementText = page.getByText('User Profile Management', { exact: true });
+   await expect(userProfileManagementText).toBeVisible();  
+   await page.getByRole('row', { name: 'User Profile Management' }).getByRole('img').click();
+   await expect(userProfileManagementText).not.toBeVisible();
+   const recentServicesNoData = page.locator('div').filter({ hasText: /^Recent ServicesNameActionsNo Data$/ }).locator('h3', { hasText: 'No Data' });
+   await expect(recentServicesNoData).toBeVisible();
+});
+
+test('clear recent services use the Clear All Preferences btn', async ({ page }) => {
+  await page.locator('div').filter({ hasText: /^User Profile Management$/ }).click();
+  const userElement = page.getByRole('heading', { name: 'User Profile' });
+  await expect(userElement).toBeVisible();
+  await page.getByRole('link', { name: 'Preferences' }).click();
+  const userPreferencesElement = page.getByRole('heading', { name: 'User Preferences' });
+  await expect(userPreferencesElement).toBeVisible();
+  const userProfileManagementText = page.getByText('User Profile Management', { exact: true });
+  await expect(userProfileManagementText).toBeVisible(); 
+  await page.locator('ifx-button').filter({ hasText: 'Clear All Preferences' }).locator('a').click();
+  await expect(userProfileManagementText).not.toBeVisible();
+  const recentServicesNoData = page.locator('div').filter({ hasText: /^Recent ServicesNameActionsNo Data$/ }).locator('h3', { hasText: 'No Data' });
+  await expect(recentServicesNoData).toBeVisible();
+});
 
 
   test('create and delete an API Key', async ({ page }) => {
     await page.locator('div').filter({ hasText: /^User Profile Management$/ }).click();
+    const userElement = page.getByRole('heading', { name: 'User Profile' });
+    await expect(userElement).toBeVisible();
     await page.getByText('Api keys:').click();
+    const keyElement = page.getByRole('heading', { name: 'User API Keys' });
+    await expect(keyElement).toBeVisible();
     await page.locator('ifx-button a').click();
     await page.getByTestId('app-main-container').getByRole('textbox').fill('2024-09-20');
     await page.locator('ifx-button').filter({ hasText: 'Cancel' }).locator('a').click();
@@ -59,7 +120,7 @@ test.describe('<Accounts>', () => {
     await page.locator('.tooltip-container > .hydrated > .inline-svg > path').first().click();
     await page.locator('div:nth-child(5) > .api-key-box-copy > .tooltip-container > .hydrated > .inline-svg > path').click();
     await page.locator('ifx-button a').click();
-     // check that the Add API Key button disabled (but this check doesn't pass)
+     // check that the Add API Key button disabled (but this point doesn't pass)
     //await expect(page.getByTestId('add-user-btn')).toHaveAttribute('disabled', 'true');
     await page.getByTestId('copy-public-key-button').getByRole('img').click();
     await page.getByTestId('delete-api-key').getByRole('img').click();
@@ -76,6 +137,9 @@ test.describe('<Accounts>', () => {
     await page.getByTestId('confirmation-input').getByPlaceholder('Confirm deleting').fill('DELETE');
     await page.getByTestId('delete').locator('a').click();
     await page.waitForTimeout(2000);
+    const cellElement = page.getByRole('cell', { name: 'No Data' });
+    const divInsideCell = cellElement.locator('div');
+    await expect(divInsideCell).toBeVisible();
     await page.getByRole('navigation').getByRole('link', { name: 'Home' }).click();
   });
 
